9be90a7d13a96fbe4b0f37207500c458de6ed186
Aman2221 <as562770@gmail.com>
2024-12-16 13:42:28 +0400

    optimized the JS for the best

--- ToDo/main.js
+++ ToDo/main.js
@@ -7,39 +7,47 @@ let currentItemToUpdate = null;
 todoForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const todoValue = todoInput.value.trim();
-  if (addBtn.textContent == "Update") {
-    const spanEle = currentItemToUpdate.querySelector("span");
-    spanEle.textContent = todoValue;
-    addBtn.textContent = "Add";
-  } else {
-    const todoValueWithoutSpace = todoInput.value.replaceAll(" ", "_");
-    const randomNumber = Math.floor(100000 + Math.random() * 900000);
-    const listElement = document.createElement("li");
-    listElement.classList = `${todoValueWithoutSpace}_${randomNumber} w-full flex justify-between items-center mt-3`;
-    listElement.innerHTML = `<span class=text-base">${todoValue}</span>
-    <div class="flex gap-2">
-      <button class="edit_btn_${randomNumber} border border-yellow-400 text-yellow-400 p-1 rounded">Edit</button>
-      <button class="remove_btn_${randomNumber} border border-red-400 text-red-400 p-1 rounded">Remove</button>
-    </div>`;
-
-    todoList.appendChild(listElement);
-
-    //after adding list element to DOM to for the edit todo item
-    listElement
-      .querySelector(`.edit_btn_${randomNumber}`) //getting the edit button fo the item
-      .addEventListener("click", () => {
-        currentItemToUpdate = listElement;
-        addBtn.textContent = "Update";
-        todoInput.value = listElement.querySelector("span").textContent;
-      });
-
-    //after adding list element to DOM to remove the item on click of remove button
-    listElement
-      .querySelector(`.remove_btn_${randomNumber}`)
-      .addEventListener("click", function () {
-        listElement.remove();
-      });
-  }
-
-  todoInput.value = "";
+  (currentItemToUpdate ? updateItem : addItem)(todoValue);
+  resetForm();
 });
+
+const addItem = (todoValue) => {
+  const listElement = document.createElement("li");
+  listElement.className = `w-full flex justify-between items-center mt-3`;
+
+  // I thought i need to give unique class name or id to button every time new list item added but there is no need as javascript added to event listener to the current item buttons and handles it in the backgournd
+
+  listElement.innerHTML = `<span class=text-base">${todoValue}</span>
+  <div class="flex gap-2">
+    <button class="edit_btn border border-yellow-400 text-yellow-400 p-1 rounded">Edit</button>
+    <button class="remove_btn border border-red-400 text-red-400 p-1 rounded">Remove</button>
+  </div>`;
+
+  todoList.appendChild(listElement);
+
+  //after adding list element to DOM for the edit todo item
+  listElement
+    .querySelector(`.edit_btn`) //getting the edit button fo the item
+    .addEventListener("click", () => {
+      currentItemToUpdate = listElement;
+      addBtn.textContent = "Update";
+      todoInput.value = listElement.querySelector("span").textContent;
+    });
+
+  //after adding list element to DOM to remove the item on click of remove button
+  listElement
+    .querySelector(`.remove_btn`)
+    .addEventListener("click", function () {
+      listElement.remove();
+    });
+};
+
+const updateItem = (todoValue) => {
+  currentItemToUpdate.querySelector("span").textContent = todoValue;
+};
+
+function resetForm() {
+  todoInput.value = "";
+  addBtn.textContent = "Add";
+  currentItemToUpdate = null;
+}
